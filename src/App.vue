782e47c65ebd09468d859158bfb52c8935d84647
unknown <af5909704@gmail.com>
2022-08-18 19:34:53 +0700

    task card component

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+
+  <div class="container">
+    <CardVue :tasks="task"/>
+  </div>
+
+</template>
+
+<script>
+import CardVue from './components/Card.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CardVue
+  },
+  data() {
+    return {
+      task: {
+        category: "Cat",
+        task_text: "to do sth",
+        timeline: "23/08/2020",
+        cost: "250USD"
+      }
+      
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 5%;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="task_card">
+
+        <div class="ch_box">
+            <span>{{ tasks.category }}</span>
+            <button></button>
+        </div>
+
+        <div  class="task_name">
+            <span>{{ tasks.task_text }}</span>
+        </div>
+
+        <div class="task_desc">
+            <p>Timeline: {{ tasks.timeline }}</p>
+            <p>Cost: {{ tasks.cost }}</p>
+        </div>
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'CardVue',
+    props: {
+        tasks: {
+            category: String,
+            task_text: String,
+            timeline: String,
+            cost: String
+        }
+    }
+}
+</script>
+<style>
+.task_card {
+    min-width: 300px;
+    background-color: #EDF8FF;
+    border-radius: 15px;
+    text-align: left;
+    padding: 5px;
+    margin: 10px;
+}
+
+.task_card:hover{
+    box-shadow: 10px 5px 5px rgb(180, 173, 173);
+    transition: .5s;
+}
+
+.ch_box {
+    display: flex;
+    align-items: center;
+    margin: 5px 0;
+    justify-content: space-between;
+}
+
+.ch_box span,
+button {
+    text-align: left;
+    margin: 5px 10px;
+}
+
+.ch_box button {
+    width: 20px;
+    height: 20px;
+    border: 2px solid #838080;
+    border-radius: 5px;
+    background-color: #fff;
+}
+
+.ch_box button:hover{
+    background-color: rgb(81, 127, 234);
+    transition: .5s;
+}
+
+.ch_box span {
+    border: 1px solid black;
+    border-radius: 12px;
+    padding: 2px 15px;
+}
+
+.task_name {
+    padding: 10px;
+    font-size: 18px;
+}
+
+.task_desc {
+    margin: 5px 10px;
+}
+
+.task_desc p{
+    margin: 10px 0;
+    color: #838080;
+    font-size: 14px;
+}
+
+</style>
